28712d346a7f50b7cbb109646ce5c0f8b47619d2
lawden <lawtaden@gmail.com>
2024-06-26 23:40:49 +0200

    pascal traingle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,22 +5,9 @@ def pascal_triangle(n):
     triangle = []
 
     for i in range(n):
-        # Start with a row of ones
-        row = [1] * (i + 1)
-        
-        # Fill in the intermediate values
-        for j in range(1, i):
+        row = [1] * (i + 1)  # Initialize row with 1s
+        for j in range(1, i):  # Update elements between the edges
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
-        # Add the row to the triangle
         triangle.append(row)
-    
+
     return triangle
-
-# Main script to test the function
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
