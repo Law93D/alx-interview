89f65a06a44a62b00816883d24efbdaf35f6d416
lawden <lawtaden@gmail.com>
2024-06-30 08:51:18 +0200

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,18 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle Module
+"""
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n
+
+    Args:
+        n (int): The number of rows of Pascal's triangle to generate
+
+    Returns:
+        List[List[int]]: A list of lists of integers representing Pascal's triangle
+    """
     if n <= 0:
         return []
 
