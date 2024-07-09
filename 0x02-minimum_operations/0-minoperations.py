a5cfb5d775d34a12822e35582a37a02b7c97e372
lawden <lawtaden@gmail.com>
2024-07-09 19:54:33 +0200

    min ops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,38 @@
+"""
+In a text file, there is a single character 'H'.
+Your text editor can execute only two operations in
+this file: 'copy All' and 'paste'.
+Given a number n, write a method that calculate the
+fewest number of operations needed to result in
+exactly 'n' 'H' characters in the file.
+
+program will return an integer.
+If 'n' is impossible to achieve, return '0'
+"""
+
 def minOperations(n):
     if n <= 1:
         return 0
 
-    operations = 0
-    factor = 2
+    divisor = 2
+
+    num_of_operations = 0
     
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        if n % divisor == 0:
+            print("divisor is: ", divisor)
+            n = n / divisor
+            print("\nn = n / divisor", n)
 
-    return operations
+            num_of_operations = num_of_operations = divisor
+            print("\nnum of ops", num_of_operations)
+
+        else:
+            divisor += 1
+            print("divisor after increament: ", divisor)
+
+    return num_of_operations
+
+n = 12
+
+print(minOperations(n))
